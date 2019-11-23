3942ae31862c7c52bf00aec361339d17c21710cd
Roman Geraskin Laptop <arizekaz@gmail.com>
2019-11-23 11:44:43 +0700

    1

---
+++ app/src/main/java/com/example/poketest/PokeList.java
@@ -0,0 +1,75 @@
+package com.example.poketest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PokeList {
+    private static ArrayList<Pokemon> pokeList = new ArrayList<>();
+
+    //добавление элементов в список PokeList
+    static void addElement(Pokemon poke){
+        PokeList.pokeList.add(poke);
+    }
+
+    // возвращает список PokeList
+    static ArrayList<Pokemon> getPokeList(){
+        return PokeList.pokeList;
+    }
+
+    // возвращает количество элементов списка PokeList
+    static int size(){
+        return PokeList.pokeList.size();
+    }
+
+    // удаление всех покемонов из списка PokeList
+    static void clearList(){
+        while (PokeList.size()>0) {
+            PokeList.pokeList.remove(0);
+        }
+    }
+
+    // удаление первых "countOfClear" покемонов из списка PokeList
+    static void clearPartList(int countOfClear){
+        while ((PokeList.size()-countOfClear)>0) {
+            PokeList.pokeList.remove(0);
+        }
+    }
+
+    // перестановка элементов списка PokeList
+    static void pokeSwap(int ind1,int ind2){
+        Collections.swap(PokeList.pokeList,ind1,ind2);
+    }
+
+    // возвращает индекс последнего в списке покемона
+    static int getLastIndex(){
+        if (PokeList.pokeList.size()==0){
+            return 0;
+        }else{
+            return PokeList.pokeList.get(PokeList.size()-1).getId();
+        }
+    }
+
+    // поиск покемона с максимальным заданным параметром (hp attack defence)
+    // и перестановка его на первую позицию в списке
+    static void findMax(boolean hp,boolean att, boolean def){
+        // преобразование boolean значения checkbox в int значения для сравнения
+        int hpVal = hp?1:0;
+        int attVal = att?1:0;
+        int defVal = def?1:0;
+
+        int maxParam = 0;
+        int idOfMaxPoke = 0;
+        int i = 0;
+        for(Pokemon p:pokeList){
+            if ((p.getAttack()*attVal +
+                    p.getDefence()*defVal +
+                    p.getHp()*hpVal) > maxParam){
+                maxParam = p.getAttack()*attVal + p.getDefence()*defVal + p.getHp()*hpVal;
+                idOfMaxPoke = i;
+            }
+            i++;
+        }
+        PokeList.pokeSwap(0,idOfMaxPoke);
+    }
+
+}
---
+++ app/src/main/java/com/example/poketest/Pokemon.java
@@ -0,0 +1,85 @@
+package com.example.poketest;
+
+class Pokemon{
+    private String  name;
+    private int     id;
+    private int     attack;
+    private int     defence;
+    private int     hp;
+    private int     height;
+    private int     weight;
+    private String  type;
+    private String  sprite;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(String sprite) {
+        this.sprite = sprite;
+    }
+}
